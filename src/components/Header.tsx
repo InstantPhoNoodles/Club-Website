a06fe154fed1a94b65c2c65d6095efdd44dfad31
ebarron1 <ebarron1@uw.edu>
2024-08-31 15:24:49 -0700

    worked on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,12 @@ import './Header.css'
 function Header() {
   return (
     <div className='Header'>
-      <h1>Book and Tea Club in WSU Tri-Cities</h1>
+      <div className='Header-logo'>
+        <img src="src\assets\wsu-red-white-logo.png" alt="Washington State University Logo" />
+      </div>
+      <div className='Header-banner'>
+        <h1>Book and Tea Club in WSU Tri-Cities</h1>
+      </div>
     </div>
   )
 }
