cdb7755c34d9e24dae140a4c107e5e946c0a921a
ebarron1 <ebarron1@uw.edu>
2024-09-18 00:35:55 -0700

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,10 @@ function Footer() {
   return (
     <div className='Footer'>
       <p>&copy; 2024 Book and Tea Club</p>
+      <p>
+        Disclaimer: This is an unofficial website for the Book and Teal Club at WSU Tri-Cities.
+        We are not an official website commissioned by Washington State University.
+      </p>
     </div>
   )
 }
