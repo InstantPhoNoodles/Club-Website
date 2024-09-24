fb3169420feb9e0e99a2c10056c231d0bde427a3
ebarron1 <ebarron1@uw.edu>
2024-09-23 23:51:44 -0700

    worked on socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,18 @@ function Footer() {
         Disclaimer: This is an unofficial website for the Book and Teal Club at WSU Tri-Cities.
         We are not an official website commissioned by Washington State University.
       </p>
+      <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+        <img src="src\assets\facebook.png" alt="Facebook"/>
+        WSUTC_BookAndTeaClub
+      </a>
+      <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+        <img src="src\assets\instagram.png" alt="Instagram"/>
+        WSUTC_BookAndTeaClub
+      </a>
+      <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+        <img src="src\assets\twitter-blue.png" alt="Twitter"/>
+        WSUTC_BookAndTeaClub
+      </a>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,10 @@ const Home: React.FC = () => {
   return (
     <div>
       <Header/>
+
+      {/* Home Page Content */}
       <div className='home'>
+        {/* About us and Anouncment */}
         <section className='home-info'>
           <div className='about-us'>
             <h2>About Us</h2>
@@ -24,15 +27,55 @@ const Home: React.FC = () => {
             </p>
           </div>
         </section>
-        <section className='socials'>
-          <div>
-            <h2>Follow Us On Our Social Media</h2>
+
+        {/* Club Officiers Section */}
+        <section className='club-officers'>
+          <h2>Meet Our Officers</h2>
+          <div className="officer-grid">
+            <div className="officer">
+              <img src="officer1.jpg" alt="Officer 1" />
+              <p>Officer 1</p>
+            </div>
+            <div className="officer">
+              <img src="officer2.jpg" alt="Officer 2" />
+              <p>Officer 2</p>
+            </div>
+            <div className="officer">
+              <img src="officer3.jpg" alt="Officer 3" />
+              <p>Officer 3</p>
+            </div>
+            <div className="officer">
+              <img src="officer4.jpg" alt="Officer 4" />
+              <p>Officer 4</p>
+            </div>
+            <div className="officer">
+              <img src="officer5.jpg" alt="Officer 5" />
+              <p>Officer 5</p>
+            </div>
+            <div className="officer">
+              <img src="officer6.jpg" alt="Officer 6" />
+              <p>Officer 6</p>
+            </div>
           </div>
         </section>
-        <section className='club-officers'>
-          <h2>Club Officers</h2>
+
+        {/* Club Social Media Section */}
+        <section className="socials">
+          <h2>Follow Us On Our Social Media</h2>
+          <div className="social-icons">
+            <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+              <img src="src\assets\facebook.png" alt="Facebook"/>
+            </a>
+            <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+              <img src="src\assets\instagram.png" alt="Instagram"/>
+            </a>
+            <a href="https://www.instagram.com/wsutc_bookandteaclub/" target="_blank">
+              <img src="src\assets\twitter.png" alt="Twitter"/>
+            </a>
+          </div>
         </section>
       </div>
+
       <Footer/>
     </div>
   );
