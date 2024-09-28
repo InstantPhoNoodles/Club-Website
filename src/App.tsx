ac4d6d3198f6c51a15f61b07cc511a87ee1f5f97
ebarron1 <ebarron1@uw.edu>
2024-09-28 00:59:19 -0700

    added browserrouter basename to app.tsx and added back wsu-bg2.png

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Posts from './pages/Posts';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home route */}
-        <Route path="/about" element={<About />} /> {/* About route */}
-        <Route path="/posts" element={<Posts />} /> {/* Posts route */}
-      </Routes>
-    </div>
+    <BrowserRouter basename="/Club-Website"> 
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home route */}
+          <Route path="/about" element={<About />} /> {/* About route */}
+          <Route path="/posts" element={<Posts />} /> {/* Posts route */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
