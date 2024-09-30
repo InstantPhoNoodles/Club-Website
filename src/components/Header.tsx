06a262b9beb2a9549e6424fdf1f5865718494114
ebarron1 <ebarron1@uw.edu>
2024-09-30 16:04:51 -0700

    added a hamburger menus icon when in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
 import './Header.css'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './Navbar';
 import wsuLogo from '../assets/wsu-logo.png';
 
 const Header: React.FC = () => {
+  const [showNavbar, setShowNavbar] = useState(false); // State to control showing the OfficerCard
+
+  const handleClick = () => {
+    setShowNavbar(!showNavbar);
+  };
+
   return (
     <div className='Header'>
       <div className='wsu-logo'>
@@ -13,8 +21,13 @@ const Header: React.FC = () => {
       <div className='banner'>
         <Link to="/"><h1>Book and Tea Club at WSU Tri-Cities</h1></Link>
       </div>
+      <div className='hamburger' onClick={handleClick}> { }
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+        </svg>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header
