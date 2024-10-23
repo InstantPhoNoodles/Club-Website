83e9b9d7b8518530c0d476aea56e6d8066d51253
ebarron1 <ebarron1@uw.edu>
2024-10-23 13:37:55 -0700

    added full functionality to hamburger menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+  show?: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ show }) => {
   return (
-    <div className='Navbar'>
+    <div className={`Navbar ${show ? 'show' : ''}`}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/posts">Posts</Link>
